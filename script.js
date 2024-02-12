e62ce7ecaf2f5bf37121c067f4724192f1939c60
minaxcvii <jnlchvs@gmail.com>
2024-02-13 01:45:37 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function casio(){
+    var rand1 = Math.floor(Math.random() * 500);
+    var rand2 = Math.floor(Math.random() * 500);
+  document.getElementById("No").style.marginTop =  (rand1+"px");
+    document.getElementById("No").style.marginLeft =  (rand2+"px");
+  }
